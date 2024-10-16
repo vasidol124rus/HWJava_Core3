341c8c067110390a52ac176c17c8949a1aee3f20
Vasiliy Kuznetsov <vasekku25@gmail.com>
2024-10-16 19:37:17 +0700

    Добавил класс работник Employee

---
+++ src/Employee.java
@@ -0,0 +1,15 @@
+class Employee {
+    int year;
+    int month;
+    int day;
+
+    public Employee(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public boolean compareDates(Employee other) {
+        return this.year == other.year && this.month == other.month && this.day == other.day;
+    }
+}
