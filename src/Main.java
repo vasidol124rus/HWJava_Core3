cf283273624b14331e44e4199c7f10d86e75a440
Vasiliy Kuznetsov <vasekku25@gmail.com>
2024-10-16 19:41:28 +0700

    Добавил точку входа

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Employee emp1 = new Employee(1990, 5, 15);
+        Manager manager = new Manager(1975, 10, 25);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Employee[] employees = {emp1, manager};
+
+        Manager.increaseSalary(employees, 1000);
+
+        for (Employee emp : employees) {
+            System.out.println("Employee's salary: " + emp.year + "-" + emp.month + "-" + emp.day);
         }
     }
 }
